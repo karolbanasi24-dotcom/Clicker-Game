b85c2abc4242060abd2714ef364f3cd04254f428
karolbanasi24-dotcom <karolbanasi24@gmail.com>
2026-05-25 15:19:46 +0200

    Update GameMenager.cs

--- Assets/Scripts/GameMenager.cs
+++ Assets/Scripts/GameMenager.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameMenager : MonoBehaviour
 {
+    [Header("UI")]
+    [SerializeField] private TextMeshProUGUI scoreText;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
