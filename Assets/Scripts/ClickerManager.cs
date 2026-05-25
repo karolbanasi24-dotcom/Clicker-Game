0d1d5697ae7db73f24921a1518752c87320bae87
karolbanasi24-dotcom <karolbanasi24@gmail.com>
2026-05-25 16:16:48 +0200

    s

--- Assets/Scripts/ClickerManager.cs
+++ Assets/Scripts/ClickerManager.cs
@@ -1,16 +1,24 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ClickerManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("References")]
+    [SerializeField] private ClickerUI clickerUI;
+    [SerializeField] private Button Apple;
+    private int score = 0;
+    private int pointsPerClick = 1;
+
+    private void Start()
     {
-        
+        clickerUI.UdateUI(score);
+        Apple.onClick.AddListener(AddScore);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void AddScore()
     {
-        
+        score += pointsPerClick;
+        clickerUI.UdateUI(score);
     }
 }
